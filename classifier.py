3d87fd1854bb827447a51a9e6e7e8545b4a1562b
smaityumich <56092728+smaityumich@users.noreply.github.com>
2020-04-30 12:25:42 -0400

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -6,9 +6,18 @@ from sklearn import linear_model
 seed = 1
 dataset_orig_train, dataset_orig_test = preprocess_adult_data(seed = seed)
 
-## Run linear regression X_gender_train on y_gender_train to get the sensitive direction for the protected attribute gender
 x_unprotected_train, x_protected_train = dataset_orig_train.features[:, :39], dataset_orig_train.features[:, 39:]
 x_unprotected_test, x_protected_test = dataset_orig_test.features[:, :39], dataset_orig_test.features[:, 39:]
+y_train, y_test = dataset_orig_train.labels, dataset_orig_test.labels
+
+
+
+
+
+
+
+## Run linear regression X_gender_train on y_gender_train to get the sensitive direction for the protected attribute gender
+
 protected_regression = linear_model.LinearRegression(fit_intercept = False)
 protected_regression.fit(x_unprotected_train, x_protected_train)
 sensetive_directions = protected_regression.coef_
