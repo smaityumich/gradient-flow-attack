e22ad51e5fba89738608fc21e8cf64a41df03bbe
Subha Maity <smaity@smaity-mbp.local>
2020-05-27 11:53:45 -0400

    modified:   utils.py

--- myapproach/utils.py
+++ myapproach/utils.py
@@ -29,42 +29,22 @@ def _accuracy(y, ypred):
 
 
 
-def gram_schmidt(X):
-    """
-    Implements Gram-Schmidt orthogonalization.
 
-    Parameters
-    ----------
-    X : an n x k array with linearly independent columns
+def gram_schmidt(x):
+    y = []
+    for i, u in enumerate(x):
+        if i == 0:
+            while np.linalg.norm(u) != 1:
+                u = u/np.linalg.norm(u)
+            y.append(u)
+        else:
+            while np.sum([np.absolute(np.sum(u * v)) for v in y]) > 1e-16:
+                for v in y:
+                    u -= np.sum(u * v)*v
+                while  np.linalg.norm(u) != 1:
+                    u = u/np.linalg.norm(u)
+    return np.array(y)
 
-    Returns
-    -------
-    U : an n x k array with orthonormal columns
-
-    """
-
-    # Set up
-    n, k = X.shape
-    U = np.empty((n, k))
-    I = np.eye(n)
-
-    # The first col of U is just the normalized first col of X
-    v1 = X[:,0]
-    U[:, 0] = v1 / np.sqrt(np.sum(v1 * v1))
-
-    for i in range(1, k):
-        # Set up
-        b = X[:, i]       # The vector we're going to project
-        Z = X[:, 0:i]     # First i-1 columns of X
-
-        # Project onto the orthogonal complement of the col span of Z
-        M = I - Z @ np.linalg.inv(Z.T @ Z) @ Z.T
-        u = M @ b
-
-        # Normalize
-        U[:, i] = u / np.sqrt(np.sum(u * u))
-
-    return U
 
 def projection_matrix(sensetive_directions):
     orthogonal_sd = scipy.linalg.orth(sensetive_directions.T).T
