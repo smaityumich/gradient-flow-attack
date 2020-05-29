5f854b2be34ec7b1d7dcc0b0f11714758447621f
Subha Maity <smaity@smaity-mbp.local>
2020-05-29 08:19:29 -0400

    modified:   ../../2d-simulation/loss_ratio.py modified:   ../../2d-simulation/test.ipynb

--- 2d-simulation/loss_ratio.py
+++ 2d-simulation/loss_ratio.py
@@ -22,42 +22,57 @@ def fair_metric_fn(theta):
         return tf.norm(x @ theta, axis = 1)
     return fair_metric
 
-def sample_perturbation(x, y, classifier, fair_metric, regularizer = 5, learning_rate = 2e-2, num_steps = 200):
+def sample_perturbation(data, classifier, fair_metric, regularizer = 5, learning_rate = 2e-2, num_steps = 200):
+    x, y = data
     x = tf.reshape(x, (1, -1))
     y = tf.reshape(y, (1, -1))
     x_start = x
     x_fair = x
+
     #x += tf.cast(np.random.normal(size=(1, 39)), dtype = tf.float32)*1e-9
-    for _ in range(num_steps):
+    for i in range(num_steps):
         with tf.GradientTape() as g:
             g.watch(x_fair)
             prob = classifier(x_fair)
             loss = utils.EntropyLoss(y, prob)  - regularizer * tf.reduce_sum(fair_metric(x_fair - x_start)**2)
-
+        
         gradient = g.gradient(loss, x_fair)
-        x_fair = x_fair + learning_rate * gradient#utils.protected_direction(gradient, sensetive_directions)
+        if i == 0:
+            x_fair0, x_fair = x_fair, x_fair + learning_rate * gradient#utils.protected_direction(gradient, sensetive_directions)
+        else:
+            if not tf.reduce_all(tf.math.is_finite(loss)):
+                x_fair = x_fair0
+                break
+            x_fair0, x_fair = x_fair, x_fair + learning_rate * gradient
 
     ratio = utils.EntropyLoss(y, classifier(x_fair)) / utils.EntropyLoss(y, classifier(x_start))
     return ratio.numpy()
 
 
-def sample_perturbation_l2_base(x, y, classifier, fair_metric, regularizer = 5, learning_rate = 2e-2, num_steps = 200):
+def sample_perturbation_l2_base(data, classifier, fair_metric, regularizer = 5, learning_rate = 2e-2, num_steps = 200):
+    x, y = data
     x = tf.reshape(x, (1, -1))
     y = tf.reshape(y, (1, -1))
     x_start = x
     x_fair = x
     x_base = x
     #x += tf.cast(np.random.normal(size=(1, 39)), dtype = tf.float32)*1e-9
-    for _ in range(num_steps):
+    for i in range(num_steps):
         with tf.GradientTape() as g:
             g.watch(x_fair)
             prob = classifier(x_fair)
             loss = utils.EntropyLoss(y, prob)  - regularizer * tf.reduce_sum(fair_metric(x_fair - x_start)**2)
 
         gradient = g.gradient(loss, x_fair)
-        x_fair = x_fair + learning_rate * gradient#utils.protected_direction(gradient, sensetive_directions)
+        if i == 0:
+            x_fair0, x_fair = x_fair, x_fair + learning_rate * gradient#utils.protected_direction(gradient, sensetive_directions)
+        else:
+            if not tf.reduce_all(tf.math.is_finite(loss)):
+                x_fair = x_fair0
+                break
+            x_fair0, x_fair = x_fair, x_fair + learning_rate * gradient
 
-    for _ in range(num_steps):
+    for i in range(num_steps):
         with tf.GradientTape() as g:
             g.watch(x_base)
             prob = classifier(x_base)
@@ -65,7 +80,13 @@ def sample_perturbation_l2_base(x, y, classifier, fair_metric, regularizer = 5, 
             loss = utils.EntropyLoss(y, prob)  - regularizer * tf.norm(perturb)**2
 
         gradient = g.gradient(loss, x_base)
-        x_base = x_base + learning_rate * gradient
+        if i == 0:
+            x_base0, x_base = x_base, x_base + learning_rate * gradient#utils.protected_direction(gradient, sensetive_directions)
+        else:
+            if not tf.reduce_all(tf.math.is_finite(loss)):
+                x_base = x_base0
+                break
+            x_base0, x_base = x_base, x_base + learning_rate * gradient
 
     
 
@@ -84,12 +105,11 @@ def mean_ratio(theta, fair_direction, regularizer = 1, learning_rate = 5e-2, num
     while np.linalg.norm(fair_direction) != 1:
         fair_direction = fair_direction / np.linalg.norm(fair_direction)
     fair_metric = fair_metric_fn(fair_direction)
-    ratios = []
-    for u, v in zip(x, y):
-        r = sample_perturbation(u, v, classifier  = cl, fair_metric = fair_metric, regularizer=regularizer,\
-             learning_rate=learning_rate, num_steps=num_steps)
-
-        ratios.append(r)
+    
+    cpus = mp.cpu_count()
+    with mp.Pool(cpus) as pool:
+        ratios = pool.map(partial(sample_perturbation, classifier = cl, fair_metric = fair_metric,\
+            regularizer = regularizer, learning_rate = learning_rate, num_steps = num_steps), zip(x, y))
     print(f'Done for mean ratio of {theta}')
     return np.mean(ratios)
 
@@ -103,31 +123,38 @@ def mean_ratio_l2_base(theta, fair_direction, regularizer = 1, learning_rate = 5
     while np.linalg.norm(fair_direction) != 1:
         fair_direction = fair_direction / np.linalg.norm(fair_direction)
     fair_metric = fair_metric_fn(fair_direction)
-    ratios = []
-    for u, v in zip(x, y):
-        r = sample_perturbation_l2_base(u, v, classifier  = cl, fair_metric = fair_metric, regularizer=regularizer,\
-             learning_rate=learning_rate, num_steps=num_steps)
-
-        ratios.append(r)
+    cpus = mp.cpu_count()
+    with mp.Pool(cpus) as pool:
+        ratios = pool.map(partial(sample_perturbation_l2_base, classifier = cl, fair_metric = fair_metric,\
+            regularizer = regularizer, learning_rate = learning_rate, num_steps = num_steps), zip(x, y))
     print(f'Done for mean ratio l2 base of {theta}')
     return np.mean(ratios)
 
 
-theta1 = np.arange(0, 5, step = 0.2)
-theta2 = np.arange(0, 5, step= 0.2)
+theta1 = np.arange(0, 4.3, step = 0.4)
+theta2 = np.arange(0, 4.3, step= 0.4)
 thetas = itertools.product(theta1, theta2)
 theta = [list(i) for i in thetas]
 
 fair_direction = [0, 1]
 
-cpus = mp.cpu_count()
-print(f'Number of cpus {cpus}')
-with mp.Pool(cpus) as pool:
-    mean_ratio_theta = pool.map(partial(mean_ratio, fair_direction = fair_direction, regularizer = 1,\
-        learning_rate = 5e-2, num_steps = 100), theta)
 
-    mean_ratio_theta_l2_base = pool.map(partial(mean_ratio_l2_base, fair_direction = fair_direction, regularizer = 1,\
-        learning_rate = 5e-2, num_steps = 100), theta)
+
+mean_ratio_theta = []
+mean_ratio_theta_l2_base = []
+for t1 in theta1:
+    mean_ratio_theta_row = []
+    mean_ratio_theta_l2_base_row = []
+    for t2 in theta2:
+        r = mean_ratio([t1, t2], fair_direction, regularizer= 1, learning_rate=2e-2, num_steps=100)
+        mean_ratio_theta_row.append(r)
+        r = mean_ratio_l2_base([t1, t2], fair_direction, regularizer= 1, learning_rate=2e-2, num_steps=100)
+        mean_ratio_theta_l2_base_row.append(r)
+    mean_ratio_theta.append(mean_ratio_theta_row)
+    mean_ratio_theta_l2_base.append(mean_ratio_theta_l2_base_row)
+
+
+
 
 np.save('data/mean_ratio_theta_fair.npy', np.array(mean_ratio_theta))
 np.save('data/mean_ratio_theta_l2_base_fair.npy', np.array(mean_ratio_theta_l2_base))
