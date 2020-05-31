ecbdb1b11a165e17e9c53291f472142e61097b8c
Subha Maity <smaity@smaity-mbp.local>
2020-05-30 20:04:16 -0400

    modified:   ../../2d-simulation/data/x.npy modified:   ../../2d-simulation/data/y.npy modified:   ../../2d-simulation/generate_data.py new file:   ../../2d-simulation/loss_linear.py modified:   ../../2d-simulation/loss_ratio.py modified:   ../../2d-simulation/test.ipynb new file:   __pycache__/metrics.cpython-37.pyc new file:   __pycache__/np_joiner.cpython-37.pyc modified:   classifier.py modified:   graph.py modified:   graph/saved_model.pb modified:   graph/variables/variables.data-00000-of-00001 new file:   metrics.py new file:   summary.py new file:   ../reduction/summary.py new file:   ../sensr/summary.py modified:   ../sensr/test.ipynb

--- 2d-simulation/generate_data.py
+++ 2d-simulation/generate_data.py
@@ -1,22 +1,22 @@
 import numpy as np
 
-def get_data(minority_group_prop = 0.2, sample_size = 400, seed = 1):
+def get_data(minority_group_prop = 0.1, sample_size = 400, seed = 1):
 
     
     mu_minority = np.array([1.5, 0])
     mu_majority = np.array([-1.5, 0])
-    w_minority  = np.array([1.5, np.sqrt(8)/3])
-    w_majority  = np.array([-1.5, np.sqrt(8)/3])
+    w_minority  = np.array([0.2, -0.01])
+    w_majority  = np.array([-0.2, -0.01])
     d = 2
-    sigma = 0.5
+    sigma = 0.25
     np.random.seed(seed)
     x = np.random.normal(0, scale=sigma, size=(sample_size,d))
     z = np.random.choice([False, True], size=sample_size, p=[minority_group_prop, 1-minority_group_prop])
     x[z,:] = x[z,:] + mu_majority
     x[~z,:] = x[~z,:] + mu_minority
     y = np.zeros(sample_size)
-#    noise = np.random.normal(size = n)
-    noise = np.zeros(sample_size)
+    noise = np.random.normal(size = (sample_size,)) * 0.01
+    #noise = np.zeros(sample_size)
     y[z] = np.sign(np.dot(x[z,:],w_majority) - np.dot(w_majority,mu_majority) + noise[z])
     y[~z] = np.sign(np.dot(x[~z,:],w_minority) - np.dot(w_minority,mu_minority) + noise[~z])
     np.save('data/x.npy', x)
---
+++ adult/baseline/summary.py
@@ -0,0 +1,51 @@
+import numpy as np
+import tensorflow as tf
+from adult_modified import preprocess_adult_data
+from sklearn import linear_model
+import utils
+import time
+import multiprocessing as mp
+import random
+import matplotlib.pyplot as plt
+import scipy
+import metrics
+plt.ioff()
+
+
+seed = 1
+tf.random.set_seed(seed)
+np.random.seed(seed)
+dataset_orig_train, dataset_orig_test = preprocess_adult_data(seed = seed)
+
+x_unprotected_train, x_protected_train = dataset_orig_train.features[:, :39], dataset_orig_train.features[:, 39:]
+x_unprotected_test, x_protected_test = dataset_orig_test.features[:, :39], dataset_orig_test.features[:, 39:]
+y_train, y_test = dataset_orig_train.labels.reshape((-1,)), dataset_orig_test.labels.reshape((-1,))
+
+
+# Casing to tensor 
+#y_train, y_test = y_train.astype('int32'), y_test.astype('int32')
+x_unprotected_train, x_unprotected_test = tf.cast(x_unprotected_train, dtype = tf.float32), tf.cast(x_unprotected_test, dtype = tf.float32)
+#y_train, y_test = tf.one_hot(y_train, 2), tf.one_hot(y_test, 2)
+
+
+
+
+graph = tf.keras.models.load_model('graph')   
+prob = graph(x_unprotected_test)
+y_pred = tf.argmax(prob, axis = 1)
+y_pred = y_pred.numpy()
+gender = dataset_orig_test.features[:, 39]
+race = dataset_orig_test.features[:, 40]
+from np_joiner import *
+print('\n\nMeasures for gender\n')
+accuracy, bal_acc, \
+            gap_rms_gen, mean_gap_gen, max_gap_gen, \
+            average_odds_difference_gen, equal_opportunity_difference_gen,\
+                 statistical_parity_difference_gen = metrics.group_metrics(y_test, y_pred, gender, label_good=1)
+
+print('\n\n\nMeasures for race\n')
+accuracy, bal_acc, \
+            gap_rms_race, mean_gap_race, max_gap_race, \
+            average_odds_difference_race, equal_opportunity_difference_race,\
+                 statistical_parity_difference_race = metrics.group_metrics(y_test, y_pred, race, label_good=1)
+
