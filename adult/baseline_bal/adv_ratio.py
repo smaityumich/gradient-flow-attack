ec8b333cb27deb89d6e7fecd1324043c26525af2
Subha Maity <smaity@smaity-mbp.local>
2020-06-01 04:32:23 -0400

    modified:   baseline_bal/adv_ratio.py modified:   create_fluctuations.py modified:   reduction/adv_ratio.py modified:   sensr/adv_ratio.py :   test.ipynb

--- adult/baseline_bal/adv_ratio.py
+++ adult/baseline_bal/adv_ratio.py
@@ -11,66 +11,6 @@ import scipy
 plt.ioff()
 import sys
 
-
-start, end = int(float(sys.argv[1])), int(float(sys.argv[2]))
-seed_data = int(float(sys.argv[3]))
-seed_model = int(float(sys.argv[4]))
-lr = float(sys.argv[5])
-tf.random.set_seed(seed_model)
-np.random.seed(seed_model)
-dataset_orig_train, dataset_orig_test = preprocess_adult_data(seed = seed_data)
-
-x_unprotected_train, x_protected_train = dataset_orig_train.features[:, :39], dataset_orig_train.features[:, 39:]
-x_unprotected_test, x_protected_test = dataset_orig_test.features[:, :39], dataset_orig_test.features[:, 39:]
-y_train, y_test = dataset_orig_train.labels.reshape((-1,)), dataset_orig_test.labels.reshape((-1,))
-
-
-
-
-
-## Running linear regression to get sensetive directions 
-
-#protected_regression = linear_model.LinearRegression(fit_intercept = False)
-#protected_regression.fit(x_unprotected_train, x_protected_train)
-#sensetive_directions = protected_regression.coef_
-
-
-
-sensetive_directions = []
-protected_regression = linear_model.LogisticRegression(fit_intercept = True)
-protected_regression.fit(x_unprotected_test, x_protected_test[:, 0])
-sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
-protected_regression.fit(x_unprotected_test, x_protected_test[:, 1])
-sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
-sensetive_directions = np.array(sensetive_directions)
-
-sensetive_directions = scipy.linalg.orth(sensetive_directions.T).T
-for i, s in enumerate(sensetive_directions):
-    while np.linalg.norm(s) != 1:
-        s = s/ np.linalg.norm(s)
-    sensetive_directions[i] = s
-
-
-
-
-
-unprotected_directions = utils.projection_matrix(sensetive_directions)
-protected_directions = utils.projection_matrix2(sensetive_directions)
-
-
-
-# Casing to tensor 
-y_train, y_test = y_train.astype('int32'), y_test.astype('int32')
-x_unprotected_train, x_unprotected_test = tf.cast(x_unprotected_train, dtype = tf.float32), tf.cast(x_unprotected_test, dtype = tf.float32)
-y_train, y_test = tf.one_hot(y_train, 2), tf.one_hot(y_test, 2)
-unprotected_directions = tf.cast(unprotected_directions, dtype = tf.float32)
-protected_directions = tf.cast(protected_directions, dtype = tf.float32)
-sensetive_directions = tf.cast(sensetive_directions, dtype = tf.float32)
-
-
-
-graph = tf.keras.models.load_model(f'baseline_bal/graphs/graph_{seed_data}_{seed_model}')               
-
 def sample_perturbation(data_point, regularizer = 20, learning_rate = 3e-2, num_steps = 200):
     x, y = data_point
     x = tf.reshape(x, (1, -1))
@@ -91,26 +31,87 @@ def sample_perturbation(data_point, regularizer = 20, learning_rate = 3e-2, num_
     
     return return_loss.numpy()
 
+if __name__ == '__main__':
+
+    start, end = int(float(sys.argv[1])), int(float(sys.argv[2]))
+    seed_data = int(float(sys.argv[3]))
+    seed_model = int(float(sys.argv[4]))
+    lr = float(sys.argv[5])
+    tf.random.set_seed(seed_model)
+    np.random.seed(seed_model)
+    dataset_orig_train, dataset_orig_test = preprocess_adult_data(seed = seed_data)
+
+    x_unprotected_train, x_protected_train = dataset_orig_train.features[:, :39], dataset_orig_train.features[:, 39:]
+    x_unprotected_test, x_protected_test = dataset_orig_test.features[:, :39], dataset_orig_test.features[:, 39:]
+    y_train, y_test = dataset_orig_train.labels.reshape((-1,)), dataset_orig_test.labels.reshape((-1,))
+
+
+
+
+
+## Running linear regression to get sensetive directions 
+
+#protected_regression = linear_model.LinearRegression(fit_intercept = False)
+#protected_regression.fit(x_unprotected_train, x_protected_train)
+#sensetive_directions = protected_regression.coef_
+
+
+
+    sensetive_directions = []
+    protected_regression = linear_model.LogisticRegression(fit_intercept = True)
+    protected_regression.fit(x_unprotected_test, x_protected_test[:, 0])
+    sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
+    protected_regression.fit(x_unprotected_test, x_protected_test[:, 1])
+    sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
+    sensetive_directions = np.array(sensetive_directions)
+
+    sensetive_directions = scipy.linalg.orth(sensetive_directions.T).T
+    for i, s in enumerate(sensetive_directions):
+        while np.linalg.norm(s) != 1:
+            s = s/ np.linalg.norm(s)
+        sensetive_directions[i] = s
+
+
+
+
+
+    unprotected_directions = utils.projection_matrix(sensetive_directions)
+    protected_directions = utils.projection_matrix2(sensetive_directions)
+
+
+
+# Casing to tensor 
+    y_train, y_test = y_train.astype('int32'), y_test.astype('int32')
+    x_unprotected_train, x_unprotected_test = tf.cast(x_unprotected_train, dtype = tf.float32), tf.cast(x_unprotected_test, dtype = tf.float32)
+    y_train, y_test = tf.one_hot(y_train, 2), tf.one_hot(y_test, 2)
+    unprotected_directions = tf.cast(unprotected_directions, dtype = tf.float32)
+    protected_directions = tf.cast(protected_directions, dtype = tf.float32)
+    sensetive_directions = tf.cast(sensetive_directions, dtype = tf.float32)
+
+
+
+    graph = tf.keras.models.load_model(f'baseline_bal/graphs/graph_{seed_data}_{seed_model}')               
+
 
 
 
 #cpus = mp.cpu_count()
 #print(f'Number of cpus : {cpus}')
 
-perturbed_test_samples = []
-for data in zip(x_unprotected_test[start:end], y_test[start:end]):
-     perturbed_test_samples.append(sample_perturbation(data, regularizer=50, learning_rate=lr, num_steps=200))
+    perturbed_test_samples = []
+    for data in zip(x_unprotected_test[start:end], y_test[start:end]):
+        perturbed_test_samples.append(sample_perturbation(data, regularizer=50, learning_rate=lr, num_steps=200))
 # with mp.Pool(cpus) as pool:
 #     perturbed_test_samples = pool.map(sample_perturbation, zip(x_unprotected_test, y_test))
 # end_time = time.time()
-perturbed_test_samples = np.array(perturbed_test_samples)
+    perturbed_test_samples = np.array(perturbed_test_samples)
 
 
 
-filename = f'baseline_bal/outcome/perturbed_ratio_start_{start}_end_{end}_seed_{seed_data}_{seed_model}_lr_{lr}.npy'
+    filename = f'baseline_bal/outcome/perturbed_ratio_start_{start}_end_{end}_seed_{seed_data}_{seed_model}_lr_{lr}.npy'
 
 
-np.save(filename, perturbed_test_samples)
+    np.save(filename, perturbed_test_samples)
 
 #input = tf.keras.Input(shape=(39,), dtype='float32', name='input')
 #output = graph.call(input)
--- adult/sensr/adv_ratio.py
+++ adult/sensr/adv_ratio.py
@@ -15,68 +15,6 @@ import json
 from tensorflow import keras
 
 
-start, end = int(float(sys.argv[1])), int(float(sys.argv[2]))
-seed_data, seed_model = int(float(sys.argv[3])), int(float(sys.argv[4]))
-lr = float(sys.argv[5])
-
-tf.random.set_seed(seed_model)
-np.random.seed(seed_model)
-dataset_orig_train, dataset_orig_test = preprocess_adult_data(seed = seed_data)
-
-x_unprotected_train, x_protected_train = dataset_orig_train.features[:, :39], dataset_orig_train.features[:, 39:]
-x_unprotected_test, x_protected_test = dataset_orig_test.features[:, :39], dataset_orig_test.features[:, 39:]
-y_train, y_test = dataset_orig_train.labels.reshape((-1,)), dataset_orig_test.labels.reshape((-1,))
-
-
-
-
-
-## Running linear regression to get sensetive directions 
-
-#protected_regression = linear_model.LinearRegression(fit_intercept = False)
-#protected_regression.fit(x_unprotected_train, x_protected_train)
-#sensetive_directions = protected_regression.coef_
-
-
-
-sensetive_directions = []
-protected_regression = linear_model.LogisticRegression(fit_intercept = True)
-protected_regression.fit(x_unprotected_test, x_protected_test[:, 0])
-sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
-protected_regression.fit(x_unprotected_test, x_protected_test[:, 1])
-sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
-sensetive_directions = np.array(sensetive_directions)
-
-sensetive_directions = scipy.linalg.orth(sensetive_directions.T).T
-for i, s in enumerate(sensetive_directions):
-    while np.linalg.norm(s) != 1:
-        s = s/ np.linalg.norm(s)
-    sensetive_directions[i] = s
-
-
-
-
-
-unprotected_directions = utils.projection_matrix(sensetive_directions)
-protected_directions = utils.projection_matrix2(sensetive_directions)
-
-
-
-# Casing to tensor 
-y_train, y_test = y_train.astype('int32'), y_test.astype('int32')
-x_unprotected_train, x_unprotected_test = tf.cast(x_unprotected_train, dtype = tf.float32), tf.cast(x_unprotected_test, dtype = tf.float32)
-y_train, y_test = tf.one_hot(y_train, 2), tf.one_hot(y_test, 2)
-unprotected_directions = tf.cast(unprotected_directions, dtype = tf.float32)
-protected_directions = tf.cast(protected_directions, dtype = tf.float32)
-sensetive_directions = tf.cast(sensetive_directions, dtype = tf.float32)
-
-
-with open(f'sensr/models/data_{seed_data}_{seed_model}.txt', 'r') as f:
-    weight = json.load(f)
-
-weights = [np.array(w) for w in weight]
-
-
 
 def SimpleDense(variable):
     w, b = variable
@@ -84,13 +22,7 @@ def SimpleDense(variable):
     b = tf.cast(b, dtype = tf.float32)
     return lambda x: tf.matmul(x, w) + b
 
-def graph(x):
-    layer1 = SimpleDense([weights[0], weights[1]])
-    layer2 = SimpleDense([weights[2], weights[3]])
-    out = tf.nn.relu(layer1(x))
-    out = layer2(out)
-    prob = tf.nn.softmax(out)
-    return prob
+
 
                 
 
@@ -114,26 +46,100 @@ def sample_perturbation(data_point, regularizer = 100, learning_rate = 5e-2, num
     
     return return_loss.numpy()
 
+if __name__ == '__main__':
+
+
+    start, end = int(float(sys.argv[1])), int(float(sys.argv[2]))
+    seed_data, seed_model = int(float(sys.argv[3])), int(float(sys.argv[4]))
+    lr = float(sys.argv[5])
+
+    tf.random.set_seed(seed_model)
+    np.random.seed(seed_model)
+    dataset_orig_train, dataset_orig_test = preprocess_adult_data(seed = seed_data)
+
+    x_unprotected_train, x_protected_train = dataset_orig_train.features[:, :39], dataset_orig_train.features[:, 39:]
+    x_unprotected_test, x_protected_test = dataset_orig_test.features[:, :39], dataset_orig_test.features[:, 39:]
+    y_train, y_test = dataset_orig_train.labels.reshape((-1,)), dataset_orig_test.labels.reshape((-1,))
+
+
+
+
+
+    ## Running linear regression to get sensetive directions 
+
+#protected_regression = linear_model.LinearRegression(fit_intercept = False)
+#protected_regression.fit(x_unprotected_train, x_protected_train)
+#sensetive_directions = protected_regression.coef_
+
+
+
+    sensetive_directions = []
+    protected_regression = linear_model.LogisticRegression(fit_intercept = True)
+    protected_regression.fit(x_unprotected_test, x_protected_test[:, 0])
+    sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
+    protected_regression.fit(x_unprotected_test, x_protected_test[:, 1])
+    sensetive_directions.append(protected_regression.coef_.reshape((-1,)))
+    sensetive_directions = np.array(sensetive_directions)
+
+    sensetive_directions = scipy.linalg.orth(sensetive_directions.T).T
+    for i, s in enumerate(sensetive_directions):
+        while np.linalg.norm(s) != 1:
+            s = s/ np.linalg.norm(s)
+        sensetive_directions[i] = s
+
+
+
+
+
+    unprotected_directions = utils.projection_matrix(sensetive_directions)
+    protected_directions = utils.projection_matrix2(sensetive_directions)
+
+
+
+# Casing to tensor 
+    y_train, y_test = y_train.astype('int32'), y_test.astype('int32')
+    x_unprotected_train, x_unprotected_test = tf.cast(x_unprotected_train, dtype = tf.float32), tf.cast(x_unprotected_test, dtype = tf.float32)
+    y_train, y_test = tf.one_hot(y_train, 2), tf.one_hot(y_test, 2)
+    unprotected_directions = tf.cast(unprotected_directions, dtype = tf.float32)
+    protected_directions = tf.cast(protected_directions, dtype = tf.float32)
+    sensetive_directions = tf.cast(sensetive_directions, dtype = tf.float32)
+
+
+    with open(f'sensr/models/data_{seed_data}_{seed_model}.txt', 'r') as f:
+        weight = json.load(f)
+
+    weights = [np.array(w) for w in weight]
+
+    def graph(x):
+        layer1 = SimpleDense([weights[0], weights[1]])
+        layer2 = SimpleDense([weights[2], weights[3]])
+        out = tf.nn.relu(layer1(x))
+        out = layer2(out)
+        prob = tf.nn.softmax(out)
+        return prob
+
+
+
 
 
 
 #cpus = mp.cpu_count()
 #print(f'Number of cpus : {cpus}')
 
-perturbed_test_samples = []
-for data in zip(x_unprotected_test[start:end], y_test[start:end]):
-     perturbed_test_samples.append(sample_perturbation(data, regularizer=50, learning_rate=lr, num_steps=200))
+    perturbed_test_samples = []
+    for data in zip(x_unprotected_test[start:end], y_test[start:end]):
+        perturbed_test_samples.append(sample_perturbation(data, regularizer=50, learning_rate=lr, num_steps=200))
 # with mp.Pool(cpus) as pool:
 #     perturbed_test_samples = pool.map(sample_perturbation, zip(x_unprotected_test, y_test))
 # end_time = time.time()
-perturbed_test_samples = np.array(perturbed_test_samples)
+    perturbed_test_samples = np.array(perturbed_test_samples)
 
 
 
-filename = f'sensr/outcome/perturbed_ratio_start_{start}_end_{end}_seed_{seed_data}_{seed_model}_lr_{lr}.npy'
+    filename = f'sensr/outcome/perturbed_ratio_start_{start}_end_{end}_seed_{seed_data}_{seed_model}_lr_{lr}.npy'
 
 
-np.save(filename, perturbed_test_samples)
+    np.save(filename, perturbed_test_samples)
 
 #input = tf.keras.Input(shape=(39,), dtype='float32', name='input')
 #output = graph.call(input)
