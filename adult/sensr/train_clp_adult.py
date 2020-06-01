f78ff05cf90512bca3cd802af8bc6dbefb511b86
Subha Maity <smaity@smaity-mbp.local>
2020-06-01 03:22:51 -0400

    modified:   adult/sensr/run_sensr_adult.py modified:   adult/sensr/train_clp_adult.py

--- adult/sensr/train_clp_adult.py
+++ adult/sensr/train_clp_adult.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.decomposition import TruncatedSVD
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 from collections import OrderedDict
 import sys
 import os
