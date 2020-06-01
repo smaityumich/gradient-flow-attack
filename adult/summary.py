07d56ccd3b65d1d1ff53f71d9ec460ee4794e738
Subha Maity <smaity@gl-login2.arc-ts.umich.edu>
2020-06-01 12:20:14 -0400

    modified:   job.sh modified:   job_summ modified:   summary.py

--- adult/summary.py
+++ adult/summary.py
@@ -25,7 +25,7 @@ def part_summary(args):
 
 if __name__ == '__main__':
     
-    expts = ['reduction', 'baseline_bal', 'sensr'] 
+    expts = ['baseline_bal', 'sensr'] 
     iteration = range(10)
     lrs = [1e-2, 1e-3, 4e-4, 1e-4]
 
