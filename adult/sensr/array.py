e5f653314c50fc0b1fe17815a5fded665e2d10e7
Subha Maity <smaity@gl-login2.arc-ts.umich.edu>
2020-06-01 02:58:51 -0400

    modified:   array.py modified:   job.sh modified:   run_sensr_adult.py

--- adult/sensr/array.py
+++ adult/sensr/array.py
@@ -7,4 +7,5 @@ seeds = np.random.randint(100000, size = (10, 2))
 i = int(float(sys.argv[1]))
 seed_data = seeds[i, 0]
 seed_model = seeds[i, 1]
+print(f'{type(i)} {seed_data} {seed_model}')
 os.system(f"python3 run_sensr_adult.py {seed_data} {seed_model}")
